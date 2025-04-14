7356781e5f0889828cc42c0e5e11e82b5c31717c
Manvi027 <manvidadhwal@gmail.com>
2025-04-14 23:38:49 +0530

    done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function App(){
+    return (
+        <div className="App">
+            <h1>Hi!! My name is Manvi Dadhwal </h1>
+            <p>Contact : [manvi@gmail.com]</p>
+        </div>
+    );
+}
+
+export default App;
